2fdf21acb6b614979b2e249fc85da35812fa0bb0
AmrMohamad <amr.mohamed124124@gmail.com>
2023-02-25 16:29:11 +0200

    added account title and username to tweet cell

--- TwttrClone/TwttrClone/Views/TweetTableViewCell.swift
+++ TwttrClone/TwttrClone/Views/TweetTableViewCell.swift
@@ -23,11 +23,27 @@ class TweetTableViewCell: UITableViewCell {
         return image
     }()
     
+    private let accountNameLabel : UILabel = {
+        let accountName = UILabel()
+        accountName.text = "Account Title"
+        accountName.font = .systemFont(ofSize: 20, weight: .medium)
+        accountName.translatesAutoresizingMaskIntoConstraints = false
+        return accountName
+    }()
     
+    private let userNameLabel : UILabel = {
+        let userName = UILabel()
+        userName.text = "@username"
+        userName.font = .systemFont(ofSize: 14, weight: .light)
+        userName.translatesAutoresizingMaskIntoConstraints = false
+        return userName
+    }()
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         contentView.addSubview(avaterImageView)
+        contentView.addSubview(accountNameLabel)
+        contentView.addSubview(userNameLabel)
         
         configureConstraints()
     }
@@ -38,12 +54,24 @@ class TweetTableViewCell: UITableViewCell {
     
     private func configureConstraints (){
         let avaterImageViewConstraints = [
-            avaterImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 15),
-            avaterImageView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 10),
+            avaterImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 12),
+            avaterImageView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 8),
             avaterImageView.heightAnchor.constraint(equalToConstant: 50),
             avaterImageView.widthAnchor.constraint(equalToConstant: 50)
         ]
         
+        let accountNameConstraints = [
+            accountNameLabel.leadingAnchor.constraint(equalTo: avaterImageView.trailingAnchor, constant: 10),
+            accountNameLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 10)
+        ]
+        
+        let userNameLabelConstraints = [
+            userNameLabel.leadingAnchor.constraint(equalTo: accountNameLabel.trailingAnchor, constant: 10),
+            userNameLabel.centerYAnchor.constraint(equalTo: accountNameLabel.centerYAnchor)
+        ]
+        
         NSLayoutConstraint.activate(avaterImageViewConstraints)
+        NSLayoutConstraint.activate(accountNameConstraints)
+        NSLayoutConstraint.activate(userNameLabelConstraints)
     }
 }
